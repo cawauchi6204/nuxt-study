203c18d01f497e7a5ea8961587217320f6a64ccc
河内達也 <cawauchi6204@gmail.com>
2024-06-17 13:40:45 +0900

    update: 最初の部分

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+const msg = ref("まだ")
+const onButtonClick = (label: string, event: Event) => {
+  const target = event.target as HTMLButtonElement
+  const text = target.innerText
+  msg.value = `${label}と${text}`
+}
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <p>{{ msg }}</p>
+  <button @click="onButtonClick('Hello', $event)">こんにちは</button>
 </template>
