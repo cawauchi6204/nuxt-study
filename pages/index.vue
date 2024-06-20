851515ce688fbf32a983ba5ff48543ebeefd1753
河内達也 <cawauchi6204@gmail.com>
2024-06-20 12:48:14 +0900

    update: refetch

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,64 @@ import type { City } from "@/interfaces";
 
 //都市情報リストをステートから取得。
 const cityList = useState<Map<number, City>>("cityList");
+//初期都市IDを大阪に設定。
+const selectedCityId = ref(1853909);
+//初期都市情報を取得。
+const selectedCityInit = cityList.value.get(selectedCityId.value) as City;
+//都市情報のテンプレート変数を用意。
+const selectedCity = ref(selectedCityInit);
+
+const runtimeConfig = useRuntimeConfig();
+const { data, pending: status, refresh } = await useAsyncData(
+	(): Promise<any> => {
+		const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
+		const params: {
+			lang: string;
+			q: string;
+			appid: string;
+		} =
+		{
+			lang: "ja",
+			q: selectedCity.value.q,
+			//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
+			appid: runtimeConfig.public.openWeatherApiKey
+		}
+		const queryParams = new URLSearchParams(params);
+		const urlFull = `${weatherInfoUrl}?${queryParams}`;
+		const response = $fetch(urlFull);
+		return response;
+	},
+	{
+		transform: (data: any): string => {
+			const weatherArray = data.weather;
+			const weather = weatherArray[0];
+			return weather.description;
+		},
+		server: true,
+
+	}
+);
+
+const onCityChanged = () => {
+	selectedCity.value = cityList.value.get(selectedCityId.value) as City;
+	refresh();
+}
 </script>
 
 <template>
 	<section>
-		<h2>都市リスト</h2>
-		<ul>
-			<li v-for="[id, city] in cityList" v-bind:key="id">
-				<NuxtLink v-bind:to="{ name: 'WeatherInfo-id', params: { id: id } }">
-					{{ city.name }}の天気
-				</NuxtLink>
-			</li>
-		</ul>
+		<label>
+			表示するお天気ポイント:
+			<select v-model="selectedCityId" v-on:change="onCityChanged">
+				<option v-for="[id, city] in cityList" v-bind:key="id" v-bind:value="id">
+					{{ city.name }}
+				</option>
+			</select>
+		</label>
+	</section>
+	<p v-if="status">データ取得中…</p>
+	<section v-else>
+		<h2>{{ selectedCity.name }}の天気</h2>
+		<p>{{ data }}</p>
 	</section>
 </template>
